470e2ecec83c0025bb27f986744bf0dcbe8cc394
acamposs <amanda.acampos2000@gmail.com>
2024-08-15 08:54:08 -0300

    Remove node_modules from the repository

---
+++ app/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <TheHeader v-show="showHeader" />
+
+  <div v-show="showName">
+    Nome: {{ fisrtname }} <br>
+    Sobrenome: {{ lastname }}
+
+  </div>
+
+  <div v-if="acessLevel === 'admin'"> Usuário Admin</div>
+  <div v-else-if="acessLevel === 'marketing'">Usuário Marketing</div>
+  <div v-else>Usuário Padrão</div>
+
+
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    TheHeader
+  },
+  data() {
+    return {
+      showHeader: true,
+      fisrtname: 'John',
+      lastname: 'Snow',
+      showName: false,
+      acessLevel: 'maketing',
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
