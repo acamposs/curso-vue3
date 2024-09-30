085c7c21cee1498380b67563e4b88c4cfedbd2ed
acamposs <amanda.campos@tc.df.gov.br>
2024-09-30 11:12:36 -0300

    adiciona layout to do list

---
+++ app/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+  './src/**/*.{html,js,vue}',
+  './public/index.html',
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
